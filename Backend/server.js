1c04692702d219529a195e09f5af768fec534e6a
Navodya Pasqual <navodyapasqual@gmail.com>
2021-08-17 17:12:10 +0530

    MongoDB connection created

---
+++ Backend/server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 8080;
+/**
+ * Get MONGODB_URI from .env
+ */
+const MONGODB_URI = process.env.MONGODB_URI;
+
+/**
+ * Connect to mongoDB using mongoose
+ */
+mongoose.connect(MONGODB_URI,{
+    useCreateIndex: true,
+    useNewUrlParser:true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+},(err) => {
+    if (err) {
+        console.log('DB error: ', err.message);
+    }
+});
+
+mongoose.connection.once('open',()=>{
+    console.log('DB Synced');
+});
+
+app.route('/').get((req, res) => {
+    res.send('SPM Group Project');
+});
+
+//API endpoints
+
+
+
+app.listen(PORT,()=>{
+    console.log(`server is up on PORT ${PORT}`);
+});
